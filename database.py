c1a51fda9c729acb627955473c0c0448096627f1
joe2003-kariz <136978187+joe2003-kariz@users.noreply.github.com>
2024-06-27 18:38:42 +0300

    Update database.py

--- database.py
+++ database.py
@@ -1,15 +1,25 @@
 import sqlite3
 
 def create_connection():
+    """
+    Create a connection to the SQLite database.
+
+    :return: Connection object.
+    """
     conn = sqlite3.connect('habits.db')
     return conn
 
 def create_tables(conn):
+    """
+    Create tables for storing habits and completions.
+
+    :param conn: Connection object.
+    """
     with conn:
         conn.execute('''CREATE TABLE IF NOT EXISTS habits (
                            id INTEGER PRIMARY KEY,
                            name TEXT,
-                           periodicity TEXT,pipi
+                           periodicity TEXT,
                            creation_date TEXT
                         )''')
         conn.execute('''CREATE TABLE IF NOT EXISTS completions (
